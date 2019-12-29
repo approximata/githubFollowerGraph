98bfc10a8b16a780f9d30e7678785fc62a984ad1
Mate Bodor <bodormate@gmail.com>
2019-12-29 22:55:51 +0100

    Init setup

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { ApolloProvider } from '@apollo/react-common';
+import { ApolloClient, InMemoryCache, HttpLink } from 'apollo-boost' 
+import { setContext } from 'apollo-link-context'
+import UserInventoryList from './components/UserInventory';
+
+const httpLink = new HttpLink({ uri: 'https://api.github.com/graphql' });
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${
+        process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+        }`
+    }
+  }
+});
+
+const link = authLink.concat(httpLink);
+
+const createApolloClient = () => (
+  new ApolloClient({
+    link: link,
+    cache: new InMemoryCache()
+  })
+);
+
+
+const App = () => {
+  const client = createApolloClient();
+
+  return (
+    <ApolloProvider client={client}>
+        <UserInventoryList/>
+    </ApolloProvider>
+  )
+
+}
+
+export default App;
